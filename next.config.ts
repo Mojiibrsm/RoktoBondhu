366c3c664fc372b76eb4402a59653a5e919ee86a
Mojib Rsm <bbcfrc@gmail.com>
2025-07-22 11:39:02 +0000

    Error: node:async_hooks Module build failed: UnhandledSchemeError: Readi

--- next.config.ts
+++ next.config.ts
@@ -21,7 +21,6 @@ const nextConfig: NextConfig = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
         config.resolve.fallback = {
-            ...config.resolve.fallback,
             'async_hooks': false,
             'fs': false,
             'tls': false,
