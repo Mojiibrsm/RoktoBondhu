f5832d7c5fdf895534e702caf1fc36c5bbb57976
Mojib Rsm <bbcfrc@gmail.com>
2025-07-22 07:51:30 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "roktobondhu-9ydo3",
-  "appId": "1:906413458872:web:db31dcf89ddc53eef43b4d",
-  "storageBucket": "roktobondhu-9ydo3.firebasestorage.app",
-  "apiKey": "AIzaSyB_4laymJYn12YDRDq_Smrnl7KHPdaeVYI",
-  "authDomain": "roktobondhu-9ydo3.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "906413458872"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 // Initialize Firebase
